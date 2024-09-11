ef3525a8132b6f871aecadc49ffe2cd493012f80
andria-op <andriakolxi3@gmail.com>
2024-09-11 16:13:34 +0400

    first commit

---
+++ day2/lesson2.py
@@ -0,0 +1,6 @@
+name = "andria"
+age = 12 
+height = 155.3
+username = "shelia"
+
+print("my name is " + name + "," + " my username is " + username + ", " + "i am " + str(age) + " years old" + " and i am " + str(height) + " cm")
